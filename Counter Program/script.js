96d99546c5d95327b091da333113eec31109f52e
Roshan Roy <roshanroy1121@gmail.com>
2025-01-15 16:18:39 +0530

    Added Counter Program folder

---
+++ Counter Program/script.js
@@ -0,0 +1,20 @@
+let decreaseBtn = document.getElementById("decreaseBtn");
+let increaseBtn = document.getElementById("increaseBtn");
+let resetBtn = document.getElementById("resetBtn");
+let countLabel = document.getElementById("countLabel");
+let count = 0;
+
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
