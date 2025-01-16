4d197b587f890cbe8664e932216001bbae4ffa86
Roshan Roy <roshanroy1121@gmail.com>
2025-01-16 16:57:24 +0530

    Added Program using ternary operator

---
+++ Program using ternary operator/script.js
@@ -0,0 +1,11 @@
+let age = 21;
+let result = age >= 18 ? "You are an adult" : "You are a minor";
+console.log(result);
+
+let time = 16;
+let greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+console.log(greeting);
+
+let isStudent = false;
+let message = isStudent ? "You are a student" : "You are not a student";
+console.log(message);
